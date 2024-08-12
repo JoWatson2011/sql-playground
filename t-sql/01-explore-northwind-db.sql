e44946b63df5f575aaf1df6af9b4725e3222a532
jowatson2011 <jowatson@protonmail.com>
2024-08-12 10:52:29 +0100

    Queried tables and explored database stucture

--- t-sql/01-explore-northwind-db.sql
+++ t-sql/01-explore-northwind-db.sql
@@ -1,10 +1,18 @@
 USE Northwind
 GO
 
+-- View summary information of all objects in the database
 sp_help
+-- EXEC sp_help
 
-GO
+PRINT 'Orders:'
+SELECT * FROM dbo.Orders
+-- Reports indexes on a table
+EXEC sp_helpindex N'dbo.Orders'
 
-SELECT * FROM Orders
--- View from 
-SELECT * FROM [Order Details]
+-- Syntax for name with whitespace
+SELECT * FROM dbo.[Order Details]
+
+-- Query with alias'
+SELECT CompanyName, ContactName, ContactTitle, PostalCode as 'Zip Code' 
+FROM dbo.customers AS cust
